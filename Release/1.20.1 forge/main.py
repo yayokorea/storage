e7c9be40ac841c91ed5684dfc7d0c18bf68503d9
Yayo <jiwoowatch@gmail.com>
2024-05-26 02:05:58 +0900

    전체 로직 변경
    간편설치기를 더욱 쉽게 만들 수 있도록 함수를 추가

--- Release/1.20.1 forge/main.py
+++ Release/1.20.1 forge/main.py
@@ -1,4 +1,4 @@
-#pyinstaller --uac-admin  --icon="C:\Users\user\Documents\NSIS\icon.ico" "C:\Users\user\Desktop\cxfreeze\main.py"
+#pyinstaller --uac-admin  --icon="C:\Users\user\Documents\NSIS\icon.ico" "C:\Users\user\Desktop\MinecraftInstaller\storage\Release\1.20.1 forge\main.py"
 import os
 from requests import get
 import zipfile
@@ -8,64 +8,67 @@ import requests
 temp = "C:/Windows/Temp/YAYO/"
 minecraft = f"{os.getenv('APPDATA')}/.minecraft/"
 
+github = 'https://raw.githubusercontent.com/yayokorea/storage/main/'
+storage = '1.20.1_Forge_Twilight_Forest/'
+
+file1 = '1.20.1_YAYO_Forge.zip'
+file2 = "YAYO_1.20.1.zip"
+file3 = "minecraftforge.zip"
+file4 = "minecraft.zip"
+version = "1.20.1_YAYO"
+
 if os.path.isdir(temp):
     pass
 else:
     os.mkdir(temp)
 
-
-with open(f'{minecraft}launcher_profiles.json', 'r', encoding='utf-8') as f:
-    json_data = json.load(f)
-    
-    url = 'https://raw.githubusercontent.com/yayokorea/storage/main/1.20.1_Forge_Twilight_Forest/profile.json'
-    data = requests.get(url).json()
-    
-    json_data['profiles']["1.20.1_YAYO"] = data
-
-with open(f'{minecraft}launcher_profiles.json', 'w', encoding='utf-8') as f:
-    json.dump(json_data, f, indent="\t")
-
-def download(url, file_name):
-    with open(file_name, "wb") as file:  
-        response = get(url)            
-        file.write(response.content)     
-
 if __name__ == '__main__':
-    url_1 = "https://raw.githubusercontent.com/yayokorea/storage/main/1.20.1_Forge_Twilight_Forest/1.20.1_YAYO_Forge.zip"
-    url_2 = "https://raw.githubusercontent.com/yayokorea/storage/main/1.20.1_Forge_Twilight_Forest/YAYO_1.20.1.zip"
-    url_3 = "https://raw.githubusercontent.com/yayokorea/storage/main/1.20.1_Forge_Twilight_Forest/minecraftforge.zip"
-    url_4 = "https://raw.githubusercontent.com/yayokorea/storage/main/1.20.1_Forge_Twilight_Forest/minecraft.zip"
-    
-    
-    print("1.20.1_YAYO_Forge.zip 설치중")
-    download(url_1,f"{temp}1.20.1_YAYO_Forge.zip")
-    print("YAYO_1.20.1.zip 설치중")
-    download(url_2,f"{temp}YAYO_1.20.1.zip")
-    print("minecraftforge.zip 설치중")
-    download(url_3,f"{temp}minecraftforge.zip")
-    print("minecraft.zip 설치중\n")
-    download(url_4,f"{temp}minecraft.zip")
-    
+    print('launcher_profile.json 수정중\n')
+    with open(f'{minecraft}launcher_profiles.json', 'r', encoding='utf-8') as f:
+        json_data = json.load(f)
+        url = f'{github}{storage}profile.json'
+        data = requests.get(url).json()
+        json_data['profiles'][version] = data
 
-    print("1.20.1_YAYO_Forge.zip 압축해제중")
-    zipfile.ZipFile(f'{temp}1.20.1_YAYO_Forge.zip').extractall(path='C:/Users/Public/Minecraft/')
-    print("YAYO_1.20.1.zip 압축해제중")
-    zipfile.ZipFile(f'{temp}YAYO_1.20.1.zip').extractall(path=f'{minecraft}versions/')
-    print("minecraftforge.zip 압축해제중")
-    zipfile.ZipFile(f'{temp}minecraftforge.zip').extractall(path=f'{minecraft}libraries/net/')
-    print("minecraft.zip 압축해제중\n")
-    zipfile.ZipFile(f'{temp}minecraft.zip').extractall(path=f'{minecraft}libraries/net/')
-    
-    
+    with open(f'{minecraft}launcher_profiles.json', 'w', encoding='utf-8') as f:
+        json.dump(json_data, f, indent="\t")
 
-    print("1.20.1_YAYO_Forge.zip 삭제중")
-    os.remove(f'{temp}1.20.1_YAYO_Forge.zip')
-    print("YAYO_1.20.1.zip 삭제중")
-    os.remove(f'{temp}YAYO_1.20.1.zip')
-    print("minecraftforge.zip 삭제중")
-    os.remove(f'{temp}minecraftforge.zip')
-    print("minecraft.zip 삭제중\n")
-    os.remove(f'{temp}minecraft.zip')
+    def download(url, file_name):
+        with open(file_name, "wb") as file:  
+            response = get(url)            
+            file.write(response.content)    
+
+    url_1 = f"{github}{storage}{file1}"
+    url_2 = f"{github}{storage}{file2}"
+    url_3 = f"{github}{storage}{file3}"
+    url_4 = f"{github}{storage}{file4}"
+    
+    print(f"{file1} 설치중")
+    download(url_1,f"{temp}{file1}")
+    print(f"{file2} 설치중")
+    download(url_2,f"{temp}{file2}")
+    print(f"{file3} 설치중")
+    download(url_3,f"{temp}{file3}")
+    print(f"{file4} 설치중\n")
+    download(url_4,f"{temp}{file4}")
+
+    print(f"{file1} 압축해제중")
+    zipfile.ZipFile(f'{temp}{file1}').extractall(path='C:/Users/Public/Minecraft/')
+    print(f"{file2} 압축해제중")
+    zipfile.ZipFile(f'{temp}{file2}').extractall(path=f'{minecraft}versions/')
+    print(f"{file3} 압축해제중")
+    zipfile.ZipFile(f'{temp}{file3}').extractall(path=f'{minecraft}libraries/net/')
+    print(f"{file4} 압축해제중\n")
+    zipfile.ZipFile(f'{temp}{file4}').extractall(path=f'{minecraft}libraries/net/')
+    
+    print(f"{file1} 삭제중")
+    os.remove(f'{temp}{file1}')
+    print(f"{file2} 삭제중")
+    os.remove(f'{temp}{file2}')
+    print(f"{file3} 삭제중")
+    os.remove(f'{temp}{file3}')
+    print(f"{file4} 삭제중\n")
+    os.remove(f'{temp}{file4}')
 
     print('설치 완료!\n')
     os.system('pause')
--- Release/1.20.6 fabric/main.py
+++ Release/1.20.6 fabric/main.py
@@ -1,4 +1,4 @@
-#pyinstaller --uac-admin  --icon="C:\Users\user\Documents\NSIS\icon.ico" "C:\Users\user\Desktop\cxfreeze\main.py"
+#pyinstaller --uac-admin  --icon="C:\Users\user\Documents\NSIS\icon.ico" "C:\Users\user\Desktop\MinecraftInstaller\storage\Release\1.20.6 fabric\main.py"
 import os
 from requests import get
 import zipfile
@@ -8,47 +8,51 @@ import requests
 temp = "C:/Windows/Temp/YAYO/"
 minecraft = f"{os.getenv('APPDATA')}/.minecraft/"
 
+github = 'https://raw.githubusercontent.com/yayokorea/storage/main/'
+storage = '1.20.6_Fabric/'
+
+file1 = '1.20.6 Fabric Survival.zip'
+file2 = "YAYO_1.20.6.zip"
+version = "1.20.6_YAYO"
+
 if os.path.isdir(temp):
     pass
 else:
     os.mkdir(temp)
 
-
-with open(f'{minecraft}launcher_profiles.json', 'r', encoding='utf-8') as f:
-    json_data = json.load(f)
-    
-    url = 'https://raw.githubusercontent.com/yayokorea/storage/main/1.20.6_Fabric/profile.json'
-    data = requests.get(url).json()
-    
-    json_data['profiles']["1.20.1_YAYO"] = data
-
-with open(f'{minecraft}launcher_profiles.json', 'w', encoding='utf-8') as f:
-    json.dump(json_data, f, indent="\t")
-
-def download(url, file_name):
-    with open(file_name, "wb") as file:  
-        response = get(url)            
-        file.write(response.content)     
-
 if __name__ == '__main__':
-    url_1 = "https://raw.githubusercontent.com/yayokorea/storage/main/1.20.6_Fabric/1.20.6%20Fabric%20Survival.zip"
-    url_2 = "https://raw.githubusercontent.com/yayokorea/storage/main/1.20.6_Fabric/YAYO_1.20.6.zip"
-    
-    
-    print("1.20.6 Fabric Survival.zip 설치중")
-    download(url_1,f"{temp}1.20.6 Fabric Survival.zip")
-    print("YAYO_1.20.6.zip 설치중")
-    download(url_2,f"{temp}YAYO_1.20.6.zip")
+    print('launcher_profile.json 수정중\n')
+    with open(f'{minecraft}launcher_profiles.json', 'r', encoding='utf-8') as f:
+        json_data = json.load(f)
+        url = f'{github}{storage}profile.json'
+        data = requests.get(url).json()
+        json_data['profiles'][version] = data
 
-    print("1.20.6 Fabric Survival.zip 압축해제중")
-    zipfile.ZipFile(f'{temp}1.20.6 Fabric Survival.zip').extractall(path='C:/Users/Public/Minecraft/')
-    print("YAYO_1.20.6.zip 압축해제중")
-    zipfile.ZipFile(f'{temp}YAYO_1.20.6.zip').extractall(path=f'{minecraft}versions/')
+    with open(f'{minecraft}launcher_profiles.json', 'w', encoding='utf-8') as f:
+        json.dump(json_data, f, indent="\t")
 
-    print("1.20.6 Fabric Survival.zip 삭제중")
-    os.remove(f'{temp}1.20.6 Fabric Survival.zip')
-    print("YAYO_1.20.6.zip 삭제중")
-    os.remove(f'{temp}YAYO_1.20.6.zip')
+    def download(url, file_name):
+        with open(file_name, "wb") as file:  
+            response = get(url)            
+            file.write(response.content)    
+
+    url_1 = f"{github}{storage}{file1}"
+    url_2 = f"{github}{storage}{file2}"
+    
+    print(f"{file1} 설치중")
+    download(url_1,f"{temp}{file1}")
+    print(f"{file2} 설치중\n")
+    download(url_2,f"{temp}{file2}")
+
+    print(f"{file1} 압축해제중")
+    zipfile.ZipFile(f'{temp}{file1}').extractall(path='C:/Users/Public/Minecraft/')
+    print(f"{file2} 압축해제중\n")
+    zipfile.ZipFile(f'{temp}{file2}').extractall(path=f'{minecraft}versions/')
+    
+    print(f"{file1} 삭제중")
+    os.remove(f'{temp}{file1}')
+    print(f"{file2} 삭제중\n")
+    os.remove(f'{temp}{file2}')
 
     print('설치 완료!\n')
     os.system('pause')
